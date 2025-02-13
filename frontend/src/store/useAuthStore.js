83cfa6a45b9166704687fdb39837ee0a5a4d37e2
Pragun Bajracharya <pragunbaj99@gmail.com>
2025-02-13 17:38:38 -0500

    feat: removed code for testing

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -77,7 +77,7 @@ export const useAuthStore = create((set, get) => ({
 	connectSocket: () => {
 		const { authUser } = get();
 		if (!authUser || get().socket?.connected) return;
-		const socket = io(BASE_URL.replace(/\/$/, ""), {
+		const socket = io(BASE_URL, {
 			query: {
 				userId: authUser._id,
 			},
