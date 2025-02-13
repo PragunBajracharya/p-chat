a1a720dd8b320d06ce34e820303151ef5e6a77b8
Pragun Bajracharya <pragunbaj99@gmail.com>
2025-02-13 14:35:48 -0500

    feat: updated dirname

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,17 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from 'url';
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 
-dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname + "/../../.env") });
 
 
 const PORT = process.env.PORT || 5001;
@@ -27,9 +31,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/../../frontend/dist")));
 	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+		res.sendFile(path.join(__dirname, "/../../frontend/dist/index.html"));
 	});
 }
 
