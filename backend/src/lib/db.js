bcbb0a2fee7a9687789fd0ccc3586f317051223d
Pragun Bajracharya <pragunbaj99@gmail.com>
2025-02-13 15:53:28 -0500

    feat: checking db connecition

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            bufferCommands: false,  // Disable buffering
-          });
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-    }
+	try {
+		const conn = await mongoose.connect(process.env.MONGODB_URI, {
+			bufferCommands: false, // Disable buffering
+		});
+		mongoose.set("debug", true);
+		console.log(`MongoDB connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+	}
 };
